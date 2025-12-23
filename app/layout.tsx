821c40f74d3948d04508e3e9b9380be4337c218d
stong <stong@users.noreply.github.com>
2025-12-23 02:22:58 -0500

    analyticssssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script defer data-domain="senatordle.com" src="https://plausible.io/js/script.js"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
